0e2e65c6ccee4157020df9cd2d79b8db4f0d96be
(s) Matthew Gammage <mgammage@pm7-0066.uopnet.plymouth.ac.uk>
2018-11-05 11:27:19 +0000

    new

--- Assignment/sketch.js
+++ Assignment/sketch.js
@@ -1,18 +1,37 @@
-let size = 50;
 
+var beginX = 0.0;
+var beginY = 0.0;
+var endX = 685.0;
+var endY = 400.0;
+var exponent = 1;
+var step = 0.01;
+var pct = 0.0;
 
 function setup() {
-  createCanvas(500, 500);
-
+  createCanvas(720, 400);
+  noStroke();
+  distX = endX - beginX;
+  distY = endY - beginY;
 }
 
 function draw() {
-  for (let x = 0; x < 10; x++) {
-    for(let y = 0; y <10; y++){
-      rect(size*x, size*y, size, size);
-
-    randomColor = color(random(255),random(255),random(255));
-
-    fill(randomColor);}
+  fill(22, 2);
+  rect(0, 0, width, height);
+  pct += step;
+  if (pct < 1.0) {
+    x = beginX + pct * distX;
+    y = beginY + pow(pct, exponent) * distY;
   }
+  fill(270,90,45);
+  ellipse(x, y, 90, 10);
+}
+
+function mousePressed() {
+  pct = 0.0;
+  beginX = x;
+  beginY = y;
+  endX = mouseX;
+  endY = mouseY;
+  distX = endX - beginX;
+  distY = endY - beginY;
 }
