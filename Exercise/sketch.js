ea598d587b97cfcfd90c29f9cc25ac92db6f1c33
(s) Matthew Gammage <mgammage@pm7-0066.uopnet.plymouth.ac.uk>
2018-11-06 17:06:48 +0000

    new

--- Exercise/sketch.js
+++ Exercise/sketch.js
@@ -11,7 +11,15 @@ function setup() {
 function draw() {
 
   size = random(200)
+  customShape();
 
+}
+function customShape(){
   fill(random(255), random(0), random(255), random(255));
   ellipse(random(720), random(400), size, size);
+  rect(random(360), random(200), size, size);
+  ellipse(random(180), random(100), size, size);
+  rect(random(90), random(50), size, size);
+  ellipse(random(45), random(25), size, size);
+
 }
