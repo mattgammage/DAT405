f3f314ca7f889b00c0bf3e9cdfbb2d5b419dceb3
(s) Matthew Gammage <mgammage@pm7-0066.uopnet.plymouth.ac.uk>
2018-11-20 15:28:09 +0000

    Describing work

--- Assignment/sketch.js
+++ Assignment/sketch.js
@@ -1,8 +1,8 @@
 
-var beginX = 0.0;
-var beginY = 0.0;
-var endX = 700.0;
-var endY = 730.0;
+var beginX = 0.0; //coordinate for the beginning of the shape
+var beginY = 0.0; //coordinate for the beginning of the shape
+var endX = 700.0; //coordinate for the end of the shape
+var endY = 730.0; //coordinate for the end of the shape
 var exponent = 2;
 var step = 0.01;
 var pct = 0.0; //
@@ -22,11 +22,11 @@ function draw() {
     x = beginX + pct * distX;
     y = beginY + pow(pct, exponent) * distY;
   }
-  fill(random(255), random(255), random(255));
-  ellipse(x, y, 90, 10);
+  fill(random(255), random(255), random(255)); //randomises colour of shape
+  ellipse(x, y, 90, 10); //name of shape/what it is
 }
 
-function mouseMoved() {
+function mouseMoved() { //describes how to see the effect
   pct = 0.0;
   beginX = x;
   beginY = y;
