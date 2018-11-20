58349c840b2106653214f0dd936e8c49bf051e3b
(s) Matthew Gammage <mgammage@pm7-0066.uopnet.plymouth.ac.uk>
2018-11-20 17:26:31 +0000

    Describing work

--- Custom shape hw/sketch.js
+++ Custom shape hw/sketch.js
@@ -21,23 +21,23 @@ function customShape(x, y, size){
   stroke(0);
   strokeWeight(1);
   fill(12, 200, 222) //colour
-  ellipse(x, y, size, size);
+  ellipse(x, y, size, size); //shape and its size
 
   strokeWeight(5);
   stroke(99, 333, 12);
-  ellipse(x, y, size/1.2, size/1.2);
+  ellipse(x, y, size/1.2, size/1.2); //shape and its size
 
   strokeWeight(1);
   stroke(177, 178, 174);
-  ellipse(x, y, size/1.4, size/1.4);
+  ellipse(x, y, size/1.4, size/1.4); //shape and its size
 
   stroke(101, 141, 166);
   strokeWeight(5);
-  ellipse(x, y, size/1.4, size/1.4);
+  ellipse(x, y, size/1.4, size/1.4); //shape and its size
 
   noStroke();//test
   fill(111, 204, 171 ) //colour
-  ellipse(x, y, size/4, size/4);
+  ellipse(x, y, size/4, size/4); //shape and its size
 }
 function keyPressed () {
   if (key=="a" ||key=="A")customShape(xRandom, yRandom, sizeRandom)
@@ -48,43 +48,43 @@ function customShape1(x, y, size){
   stroke(0);
   strokeWeight(3);
   fill(60, 190, 20) //colour
-  rect(x, y, size, size);
+  rect(x, y, size, size); //shape and its size
 
   strokeWeight(5);
   stroke(300, 170, 100);
-  rect(x, y, size/1.2, size/1.2);
+  rect(x, y, size/1.2, size/1.2); //shape and its size
 
   strokeWeight(1);
   stroke(289, 134, 421);
-  rect(x, y, size/1.4, size/1.4);
+  rect(x, y, size/1.4, size/1.4); //shape and its size
 
   stroke(180, 45, 90);
   strokeWeight(5);
-  rect(x, y, size/1.4, size/1.4);
+  rect(x, y, size/1.4, size/1.4); //shape and its size
 
   noStroke();//test
   fill(78, 68, 234 ) //colour
-  rect(x, y, size/4, size/4);
+  rect(x, y, size/4, size/4); //shape and its size
 }
 function customShape2(x, y, size){
   stroke(0);
   strokeWeight(4);
   fill(90, 245, 180) //colour
-  triangle(x, y, size, size);
+  triangle(x, y, size, size); //shape and its size
 
   strokeWeight(4);
   stroke(180, 180, 180);
-  triangle(x, y, size/4.4, size/4.4);
+  triangle(x, y, size/4.4, size/4.4); //shape and its size
 
   strokeWeight(4);
   stroke(300, 200, 100);
-  triangle(x, y, size/1.8, size/1.8);
+  triangle(x, y, size/1.8, size/1.8); //shape and its size
 
   stroke(233, 56, 12);
   strokeWeight(4);
-  triangle(x, y, size/2.4, size/2.4);
+  triangle(x, y, size/2.4, size/2.4); //shape and its size
 
   noStroke();//test
   fill(196, 204, 171 ) //colour
-  triangle(x, y, size/4, size/4);
+  triangle(x, y, size/4, size/4); //shape and its size
 }
