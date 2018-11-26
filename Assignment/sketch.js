a4973f823705ba9f1039f265bc9f119fb04d241e
mattgammage <44403657+mattgammage@users.noreply.github.com>
2018-11-26 12:39:19 +0000

    Add files via upload
    Screenshots

--- Assignment/sketch.js
+++ Assignment/sketch.js
@@ -5,7 +5,7 @@ var endX = 700.0; //coordinate for the end of the shape on the x axis
 var endY = 730.0; //coordinate for the end of the shape on the y axis
 var exponent = 2; //determines when the shape makes its way downwards
 var step = 0.01; //speed of decent at the beginning
-var pct = 0.0; //determines where the shaoe decends from
+var pct = 0.0; //determines where the shape decends from
 
 function setup() {
   createCanvas(720, 720); //size of canvas
@@ -19,14 +19,14 @@ function draw() {
   pct += step;
   if (pct < 1.0) {
     x = beginX + pct * distX;
-    y = beginY + pow(pct, exponent) * distY;
+    y = beginY + pow(pct, exponent) * distY; //creates the logarithmic shape of the motion
   }
   fill(random(255), random(255), random(255)); //randomises colour of shape
   ellipse(x, y, 90, 10); //name of shape and its size
 }
 
 function mouseMoved() { //describes what to do in order to see effect/function
-  pct = 0.5; //speed of shapes movement with the mouse
+  pct = 0.5; //speed of shape movement with the mouse
   beginX = x;
   beginY = y;
   endX = mouseX;
