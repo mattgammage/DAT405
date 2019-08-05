50763e1af404f76483c1bb419c7607513ab15eb9
mattgammage <44403657+mattgammage@users.noreply.github.com>
2019-08-05 14:17:41 +0100

    Add files via upload

--- Asssignment part 2/Assignment 2/README.md
+++ Asssignment part 2/Assignment 2/README.md
@@ -4,8 +4,15 @@ test
 
 Assignment Part 2 DAT405
 
-For the second half of the assignment, my work contains coding that changes the colour shade of one of the three boxes on the canvas to either a brighter shade or a darker shade, depending on the data from the website that is linked in with my coding.
+For the second half of the assignment, my work contains coding that changes the colour shade of one of four thermometers on the canvas to either a brighter shade or a darker shade, depending on the data from the website that is linked in with my coding.
 
-For example, the box at the top of the page will change shades of red because I decided to have the temperature of places linked to this code. Meaning that if a the temperature is really high, then the shade of red will be brighter, or if it the temperature is really low then it will become darker, even become black if it is low enough.
+For example, the first thermometer on the left of the page will change shades of blue because I decided to have the temperature of places linked to this code. Meaning that if a the temperature is really high, then the shade of blue will be brighter, or if it the temperature is really low then it will become darker, even become black if it is low enough.
 
 https://github.com/mattgammage/DAT405
+
+
+I wanted to try adding some more specific designs to the overall code, however no matter how many times I tried, I was unable to incorporate them into my work unfortunately.
+
+https://processing.org/examples/star.html
+
+https://ianlunn.github.io/Hover/
--- Asssignment part 2/Original code/sketch.js
+++ Asssignment part 2/Original code/sketch.js
@@ -4,14 +4,12 @@
 let getLocation1 = "New Orleans";
 let getLocation2 = "New Jersey";
 let getLocation3 = "Japan";
-let getLocation4 = "France";
 //The API key has to be given by the openweathermap.org website
 let apiKey = "a93316466bc4b03baa847ec7b9006eb7";
 //This variable will store the JSON weather data
 let weather1;
 let weather2;
 let weather3;
-let weather4;
 
 //The preload function is executed before initializing the code in setup
 //Loads any related data or media files
@@ -22,13 +20,10 @@ function preload() {
   let url1 = "http://api.openweathermap.org/data/2.5/weather?q="+getLocation1+"&units=metric&appid="+apiKey;
   let url2 = "http://api.openweathermap.org/data/2.5/weather?q="+getLocation2+"&units=metric&appid="+apiKey;
   let url3 = "http://api.openweathermap.org/data/2.5/weather?q="+getLocation3+"&units=metric&appid="+apiKey;
-  let url4 = "http://api.openweathermap.org/data/2.5/weather?q="+getLocation4+"&units=metric&appid="+apiKey;
-  let url5 = "http://api.openweathermap.org/data/2.5/weather?q="+getLocation4+"&units=metric&appid="+apiKey;
   //The URL is sent to the loadJSON that returns the data to the weather variable
   weather1 = loadJSON(url1);
   weather2 = loadJSON(url2);
   weather3 = loadJSON(url3);
-  weather4 = loadJSON(url4);
 }
 
 function setup() {
@@ -39,9 +34,10 @@ function setup() {
   console.log("Temperature (max): " + weather1.main.temp_max + "°C"); //Maximum temperature
   console.log("Humidity: " + weather1.main.humidity); //Humidity
   console.log("Pressure: " + weather1.main.pressure); //Pressure
-  console.log("Wind Speed: " + weather1.wind.speed); //Wind Speed
+  console.log("Wind Speed:" + weather1.wind.speed); //Wind Speed
 
-  createCanvas(1920, 1019); //Canvas size
+
+  createCanvas(900, 900); //Canvas size
   background(160); // Overall colour of background
   textAlign(CENTER); //Position of text
   textSize(30); //Size of text
@@ -51,37 +47,20 @@ function setup() {
 function draw(){
   size1 = map(weather1.main.temp, -5, 40, 10, 100); //Determines the values between amounts
   fill(0, 0, map(weather1.main.temp,0, 20,0, 255)); //Colour of the overall shape, depends on information from website
-  ellipse(350,700,250,250) //Size of shape, in this the base of the entire design
-  rect(250,450,200,200)
-  rect(250,250,200,200)
-  rect(250,50,200,200)
+  rect(0,0,width,height/3) //Size of shape, in this case it covers one third of the canvas
   fill(255); //Colour of text
-  text("Current Temperature in New Orleans is " + weather1.main.temp + "°C", 225,650,250,250); //Position of text while still being centred
+  text("Current Temperature in New Orleans is " + weather1.main.temp + "°C", width/2, height/5); //Position of text while still being centred
 
   size2 = map(weather2.main.humidity, -5, 40, 10, 100);
   fill(map(weather2.main.humidity,0, 20,0, 255), 0, 0);
-  ellipse(650,700,250,250)
-  rect(550,450,200,200)
-  rect(550,250,200,200)
-  rect(550,50,200,200)
+  rect(0,320,width,height/3)
   fill(255);
-  text("Current Humidity in New Jersey is " + weather2.main.humidity + "%", 525,650,250,250);
+  text("Current Humidity in New Jersey is " + weather2.main.humidity + "%", width/2, height/2);
 
   size3 = map(weather3.wind.speed, 0, 20, 10, 100);
   fill(0, map(weather3.wind.speed,0, 20,0, 255), 0);
-  ellipse(950,700,250,250)
-  rect(850,450,200,200)
-  rect(850,250,200,200)
-  rect(850,50,200,200)
+  rect(0,640,width,height/3)
   fill(255);
-  text("Current Wind Speed in Japan is " + weather3.wind.speed + "m/s", 825,650,250,250);
 
-  size4 = map(weather4.main.pressure, 0, 20, 10, 100);
-  fill(200, map(weather4.main.pressure,50, 90,50, 50), 0);
-  ellipse(1250,700,250,250)
-  rect(1150,450,200,200)
-  rect(1150,250,200,200)
-  rect(1150,50,200,200)
-  fill(255);
-  text("Current Wind Speed in France is " + weather4.main.pressure + "m/s", 1125,650,250,250);
+  text("Current Wind Speed in Japan is " + weather3.wind.speed + "m/s", width/2, height/1.15);
 }
